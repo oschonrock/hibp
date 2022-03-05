398ad47616f66a53cc9a8404b88e2c66e0aefbb7
Oliver Schönrock <oliver@openbrackets.net>
2022-03-05 12:40:44 +0000

    factoring out sort
    as a database::sort mmeber function (similar to std::list::sort)

--- app/hibp_sort.cpp
+++ app/hibp_sort.cpp
@@ -1,15 +1,17 @@
 #include "flat_file.hpp"
 #include "hibp.hpp"
 #include "os/bch.hpp"
-#include <algorithm>
 #include <cstddef>
 #include <cstdlib>
 #include <filesystem>
+#include <functional>
 #include <iostream>
 #include <list>
+#include <numeric>
 #include <ranges>
 #include <stdexcept>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 int main(int argc, char* argv[]) {
@@ -17,79 +19,10 @@ int main(int argc, char* argv[]) {
     try {
         if (argc < 2) throw std::domain_error("USAGE: " + std::string(argv[0]) + " dbfile.bin");
 
-        std::string                in_filename(argv[1]);
-        std::vector<std::string>   chunk_filenames;
+        std::string                          in_filename(argv[1]);
         flat_file::database<hibp::pawned_pw> db(in_filename, 1'000);
 
-        const std::size_t max_memory_usage = 1'000'000; // ~1GB
-        std::cerr << fmt::format("{:20s} = {:12d}\n", "max_memory_usage", max_memory_usage);
-
-        std::size_t records_to_sort =
-            std::min(db.number_records(), 1'000'000UL); // limited for debug
-        // std::size_t records_to_sort = db.number_records();
-        std::cerr << fmt::format("{:20s} = {:12d}\n", "records_to_sort", records_to_sort);
-
-        std::size_t chunk_size =
-            std::min(records_to_sort, max_memory_usage / sizeof(hibp::pawned_pw));
-        std::cerr << fmt::format("{:20s} = {:12d}\n", "chunk_size", chunk_size);
-
-        std::size_t number_of_chunks = (records_to_sort / chunk_size) +
-                                       static_cast<std::size_t>(records_to_sort % chunk_size != 0);
-        std::cerr << fmt::format("{:20s} = {:12d}\n", "number_of_chunks", number_of_chunks) << "\n";
-
-        for (std::size_t chunk = 0; chunk != number_of_chunks; ++chunk) {
-            std::string chunk_filename = in_filename + ".partial." + fmt::format("{:06d}", chunk);
-            chunk_filenames.push_back(chunk_filename);
-
-            std::size_t start = chunk * chunk_size;
-            std::size_t end   = chunk * chunk_size + std::min(chunk_size, records_to_sort - start);
-            std::cerr << fmt::format("sorting [{:12d},{:12d}) => {:s}\n", start, end,
-                                     chunk_filename);
-
-            std::vector<hibp::pawned_pw> ppws;
-            std::copy(db.begin() + start, db.begin() + end, std::back_inserter(ppws));
-            std::ranges::sort(ppws, std::greater<>{}, &hibp::pawned_pw::count);
-            auto writer = flat_file::writer<hibp::pawned_pw>(chunk_filename);
-            for (const auto& ppw: ppws) writer.write(ppw);
-       }
-
-        std::string sorted_filename = in_filename + ".sorted";
-        auto        writer          = flat_file::writer<hibp::pawned_pw>(sorted_filename);
-
-        struct partial {
-            flat_file::database<hibp::pawned_pw>           db;
-            flat_file::database<hibp::pawned_pw>::iterator iter;
-            flat_file::database<hibp::pawned_pw>::iterator end;
-
-            explicit partial(std::string filename, std::size_t bufsize = 1)
-                : db(std::move(filename), bufsize), iter(db.begin()), end(db.end()) {}
-        };
-
-        // Don't use a std::vector<partial>! This will use move assignment of partials(and therefore
-        // the flat_file) during re-allocaton which WILL INVALIDATE the iterators and cause UB!
-        // flat_file ALWAYS INVALIDATES its iterators during move assignment.
-        // This can be surprising and is UNLIKE other STL containers
-        // So we use a std::list<partials> or alternatively a std::vector<std::unique_ptr<partial>>
-        std::list<partial> partials;
-        for (const auto& filename: chunk_filenames) partials.emplace_back(filename, 1000);
-
-        std::cerr << fmt::format("\nmerging [{:12d},{:12d}) => {:s}\n", 0, records_to_sort,
-                                 sorted_filename);
-
-        while (!partials.empty()) {
-            auto min = std::ranges::min_element(partials, std::greater<>{},
-                                                [](auto& partial) { return partial.iter->count; });
-            writer.write(*(min->iter));
-            ++(min->iter);
-            // during ::erase() a std::vector would, again, trigger UB, because of move
-            // assignment of flat_file which invalidates the iterators, because it leaves the ffdb_
-            // ptr back to the "parent" flat_file of the iterator dangling!
-            // this is the reason a std::deque<partial> would not work either, but a
-            // std::vector<std::unique_ptr<partial>> would be fine
-            if (min->iter == min->end) partials.erase(min);
-        }
-        for (const auto& filename: chunk_filenames)
-            std::filesystem::remove(std::filesystem::path(filename));
+        db.sort(std::greater<>{}, &hibp::pawned_pw::count);
 
     } catch (const std::exception& e) {
         std::cerr << "something went wrong: " << e.what() << "\n";
--- include/flat_file.hpp
+++ include/flat_file.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include "fmt/format.h"
+#include <algorithm>
 #include <cstddef>
 #include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <memory>
+#include <numeric>
+#include <ranges>
 #include <string>
 #include <type_traits>
 #include <vector>
@@ -16,11 +21,10 @@ namespace impl {
 
 struct ofstream_holder {
     explicit ofstream_holder(std::string dbfilename)
-        : filename_(std::move(dbfilename)), path_(filename_), ofstream_(path_, std::ios::binary) {}
+        : filename_(std::move(dbfilename)), ofstream_(filename_, std::ios::binary) {}
 
-    std::string           filename_;
-    std::filesystem::path path_;
-    std::ofstream         ofstream_;
+    std::string   filename_;
+    std::ofstream ofstream_;
 };
 
 } // namespace impl
@@ -46,6 +50,7 @@ class stream_writer {
         }
     }
 
+    // a "scoped manager" .. no special members other than destruct
     stream_writer(const stream_writer& other) = delete;
     stream_writer(stream_writer&& other)      = delete;
     stream_writer& operator=(const stream_writer& other) = delete;
@@ -63,7 +68,7 @@ class writer : private impl::ofstream_holder, public stream_writer<ValueType> {
   public:
     explicit writer(std::string dbfilename)
         : ofstream_holder(std::move(dbfilename)), stream_writer<ValueType>(ofstream_) {
-        if (!ofstream_.is_open()) throw std::domain_error("cannot open db: " + std::string(path_));
+        if (!ofstream_.is_open()) throw std::domain_error("cannot open db: " + filename_);
     }
 };
 
@@ -74,51 +79,56 @@ class database {
     static_assert(std::is_standard_layout_v<ValueType>);
 
   public:
-    explicit database(std::string dbfilename, std::size_t buf_size = 1)
-        : dbfilename_(std::move(dbfilename)), dbpath_(dbfilename_),
-          dbfsize_(std::filesystem::file_size(dbpath_)), db_(dbpath_, std::ios::binary),
-          buf_(buf_size) {
+    explicit database(std::string filename, std::size_t buf_size = 1)
+        : filename_(std::move(filename)), dbfsize_(std::filesystem::file_size(filename_)),
+          db_(filename_, std::ios::binary), buf_(buf_size) {
 
-        static_assert(std::is_move_assignable_v<database>);
+        static_assert(std::is_move_assignable_v<database>); // but that invalidates iterators!
 
         if (dbfsize_ % sizeof(ValueType) != 0)
             throw std::domain_error("db file size is not a multiple of the record size");
 
         dbsize_ = dbfsize_ / sizeof(ValueType);
 
-        if (!db_.is_open()) throw std::domain_error("cannot open db: " + std::string(dbpath_));
+        if (!db_.is_open()) throw std::domain_error("cannot open db: " + filename_);
     }
 
     using value_type = ValueType;
 
-    struct iterator {
+    struct const_iterator {
         using iterator_category = std::random_access_iterator_tag;
         using difference_type   = std::ptrdiff_t;
         using value_type        = ValueType;
-        using pointer           = ValueType*;
-        using reference         = ValueType&;
+        using pointer           = const ValueType*;
+        using reference         = const ValueType&;
 
-        iterator(database<ValueType>& ffdb, std::size_t pos) : ffdb_(&ffdb), pos_(pos) {}
+        const_iterator(database<ValueType>& ffdb, std::size_t pos) : ffdb_(&ffdb), pos_(pos) {}
 
         // clang-format off
         value_type operator*() { return current(); }
         pointer    operator->() { current(); return &cur_; }
 
-        bool operator==(const iterator& other) const { return ffdb_ == other.ffdb_ && pos_ == other.pos_; }
+        bool operator==(const const_iterator& other) const { return ffdb_ == other.ffdb_ && pos_ == other.pos_; }
         
-        iterator& operator++() { set_pos(pos_ + 1); return *this; }
-        iterator operator++(int) { iterator tmp = *this; ++(*this); return tmp; } // NOLINT why const?
-        iterator& operator--() { set_pos(pos_ - 1); return *this; }
-        iterator operator--(int) { iterator tmp = *this; --(*this); return tmp; } // NOLINT why const?
+        const_iterator& operator++() { set_pos(pos_ + 1); return *this; }
+        const_iterator operator++(int) { const_iterator tmp = *this; ++(*this); return tmp; } // NOLINT why const?
+        const_iterator& operator--() { set_pos(pos_ - 1); return *this; }
+        const_iterator operator--(int) { const_iterator tmp = *this; --(*this); return tmp; } // NOLINT why const?
 
-        iterator& operator+=(std::size_t offset) { set_pos(pos_ + offset); return *this; }
-        iterator& operator-=(std::size_t offset) { set_pos(pos_ - offset); return *this; }
+        const_iterator& operator+=(std::size_t offset) { set_pos(pos_ + offset); return *this; }
+        const_iterator& operator-=(std::size_t offset) { set_pos(pos_ - offset); return *this; }
         // clang-format on
 
-        friend iterator operator+(iterator iter, std::size_t offset) { return iter += offset; }
-        friend iterator operator+(std::size_t offset, iterator iter) { return iter += offset; }
-        friend iterator operator-(iterator iter, std::size_t offset) { return iter -= offset; }
-        friend difference_type operator-(const iterator& a, const iterator& b) {
+        friend const_iterator operator+(const_iterator iter, std::size_t offset) {
+            return iter += offset;
+        }
+        friend const_iterator operator+(std::size_t offset, const_iterator iter) {
+            return iter += offset;
+        }
+        friend const_iterator operator-(const_iterator iter, std::size_t offset) {
+            return iter -= offset;
+        }
+        friend difference_type operator-(const const_iterator& a, const const_iterator& b) {
             return static_cast<difference_type>(a.pos_ - b.pos_);
         }
 
@@ -154,15 +164,18 @@ class database {
         rec = buf_[pos - buf_start_];
     }
 
-    iterator begin() { return iterator(*this, 0); }
-    iterator end() { return iterator(*this, dbsize_); }
+    const_iterator begin() { return {*this, 0}; }
+    const_iterator end() { return {*this, dbsize_}; }
 
+    std::string filename() const { return filename_; }
     std::size_t filesize() const { return dbfsize_; }
     std::size_t number_records() const { return dbsize_; }
 
+    template <typename Comp = std::less<>, typename Proj = std::identity>
+    void sort(Comp comp = {}, Proj proj = {});
+
   private:
-    std::string            dbfilename_;
-    std::filesystem::path  dbpath_;
+    std::string            filename_;
     std::size_t            dbfsize_;
     std::size_t            dbsize_;
     std::ifstream          db_;
@@ -170,5 +183,109 @@ class database {
     std::size_t            buf_end_   = 0; // one past the end
     std::vector<ValueType> buf_;
 };
+
+namespace impl {
+
+template <typename ValueType, typename Comp = std::less<>, typename Proj = std::identity>
+std::vector<std::string> sort_chunks(database<ValueType>& db, std::size_t records_to_sort,
+                                     std::size_t number_of_chunks, std::size_t chunk_size,
+                                     Comp comp = {}, Proj proj = {}) {
+
+    std::vector<std::string> chunk_filenames;
+    chunk_filenames.reserve(number_of_chunks);
+    for (std::size_t chunk = 0; chunk != number_of_chunks; ++chunk) {
+        std::string chunk_filename = db.filename() + ".partial." + fmt::format("{:06d}", chunk);
+        chunk_filenames.push_back(chunk_filename);
+
+        std::size_t start = chunk * chunk_size;
+        std::size_t end   = chunk * chunk_size + std::min(chunk_size, records_to_sort - start);
+        std::cerr << fmt::format("sorting [{:12d},{:12d}) => {:s}\n", start, end, chunk_filename);
+
+        std::vector<ValueType> ppws;
+        std::copy(db.begin() + start, db.begin() + end, std::back_inserter(ppws));
+        std::ranges::sort(ppws, comp, proj);
+        auto part = writer<ValueType>(chunk_filename);
+        for (const auto& ppw: ppws) part.write(ppw);
+    }
+    return chunk_filenames;
+}
+
+template <typename ValueType, typename Comp = std::less<>, typename Proj = std::identity>
+void merge(const std::vector<std::string>& chunk_filenames, const std::string& sorted_filename,
+           Comp comp = {}, Proj proj = {}) {
+
+    static_assert(std::is_invocable_v<Proj, ValueType>);
+
+    auto sorted = flat_file::writer<ValueType>(sorted_filename);
+
+    struct partial {
+        flat_file::database<ValueType>                          db;
+        typename flat_file::database<ValueType>::const_iterator current;
+        typename flat_file::database<ValueType>::const_iterator end;
+
+        explicit partial(std::string filename, std::size_t bufsize = 1)
+            : db(std::move(filename), bufsize), current(db.begin()), end(db.end()) {}
+    };
+
+    // Don't use a std::vector<partial>! This will use move assignment of partials(and
+    // therefore the flat_file) during re-allocaton which WILL INVALIDATE the iterators and
+    // cause UB! flat_file ALWAYS INVALIDATES its iterators during move assignment. This can
+    // be surprising and is UNLIKE other STL containers So we use a std::list<partials> or
+    // alternatively a std::vector<std::unique_ptr<partial>>
+    std::list<partial> partials;
+    for (const auto& filename: chunk_filenames) partials.emplace_back(filename, 1000);
+
+    auto records_to_sort =
+        std::accumulate(partials.begin(), partials.end(), 0UL,
+                        [](std::size_t sum, auto& p) { return sum + p.db.number_records(); });
+
+    std::cerr << fmt::format("\nmerging [{:12d},{:12d}) => {:s}\n", 0, records_to_sort,
+                             sorted_filename);
+
+    while (!partials.empty()) {
+        auto min = std::ranges::min_element(
+            partials, comp, [&](auto& partial) { return std::invoke(proj, *partial.current); });
+
+        sorted.write(*(min->current));
+        ++(min->current);
+
+        if (min->current == min->end) partials.erase(min);
+    }
+    // for (const auto& filename: chunk_filenames)
+    //     std::filesystem::remove(std::filesystem::path(filename));
+}
+
+} // namespace impl
+
+template <typename ValueType>
+template <typename Comp, typename Proj>
+void database<ValueType>::sort(Comp comp, Proj proj) {
+    const std::size_t max_memory_usage = 100; // ~1GB
+
+    std::size_t records_to_sort = std::min(number_records(), 10UL); // limited for debug
+    // std::size_t records_to_sort = db.number_records();
+
+    std::size_t chunk_size = std::min(records_to_sort, max_memory_usage / sizeof(ValueType));
+
+    std::size_t number_of_chunks = (records_to_sort / chunk_size) +
+                                   static_cast<std::size_t>(records_to_sort % chunk_size != 0);
+
+    std::cerr << fmt::format("{:20s} = {:12d}\n", "max_memory_usage", max_memory_usage);
+    std::cerr << fmt::format("{:20s} = {:12d}\n", "records_to_sort", records_to_sort);
+    std::cerr << fmt::format("{:20s} = {:12d}\n", "chunk_size", chunk_size);
+    std::cerr << fmt::format("{:20s} = {:12d}\n", "number_of_chunks", number_of_chunks) << "\n";
+
+    std::vector<std::string> chunk_filenames =
+            impl::sort_chunks(*this, records_to_sort, number_of_chunks, chunk_size, comp, proj);
+
+    std::string sorted_filename = filename() + ".sorted";
+
+    if (number_of_chunks == 1) {
+        std::filesystem::rename(chunk_filenames[0], sorted_filename);
+        std::cerr << fmt::format("\nrenaming {:s} => {:s}\n", chunk_filenames[0], sorted_filename);
+    } else {
+        impl::merge<ValueType>(chunk_filenames, sorted_filename, comp, proj);
+    }
+}
 
 } // namespace flat_file
