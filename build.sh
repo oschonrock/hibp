3d289ec1d69ec57a982f91a88bb43f05bdf68600
Oliver Schönrock <oliver@openbrackets.net>
2022-03-15 08:00:50 +0000

    cmake: convenient way to enable tests

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@
 
 set -o errexit
 set -o nounset
-USAGE="Usage: $(basename $0) [-v | --verbose] [ gcc | clang ] [ reset | clean | debug | release | relwithdebinfo]"
+USAGE="Usage: $(basename $0) [-v | --verbose] [ gcc | clang ] [ test | reset | clean | debug | release | relwithdebinfo]"
 
 CMAKE=cmake
 BUILD=./build
 TYPE=debug
 COMPILER=clang
+TEST=
 CLEAN=
 RESET=
 VERBOSE=
@@ -23,6 +24,7 @@ for arg; do
     clang)        COMPILER=clang ;;
     gcc)          COMPILER=gcc ;;
     clean)        CLEAN=1 ;;
+    test)         TEST="-DARRCMP_TEST=ON";;
     reset)        RESET=1 ;;
     *)            echo -e "unknown option $arg\n$USAGE" >&2;  exit 1 ;;
   esac
@@ -41,7 +43,7 @@ fi
 
 [[ -n $RESET && -d $BUILD_DIR ]] && rm -rf $BUILD_DIR
     
-$CMAKE -S . -B $BUILD_DIR $COMPILER_OPTIONS -DCMAKE_BUILD_TYPE=$TYPE 
+$CMAKE -S . -B $BUILD_DIR $COMPILER_OPTIONS -DCMAKE_BUILD_TYPE=$TYPE $TEST
 
 [[ -n $CLEAN ]] && $CMAKE --build $BUILD_DIR --target clean
 
