9b95d932944d651df0c4e8b183c922cab5fbe2d9
Oliver Schönrock <oliver@openbrackets.net>
2022-03-15 10:30:05 +0000

    more cross platform build detection

--- build.sh
+++ build.sh
@@ -3,13 +3,14 @@
 
 set -o errexit
 set -o nounset
-USAGE="Usage: $(basename $0) [-v | --verbose] [ gcc | clang ] [ test | reset | clean | debug | release | relwithdebinfo]"
+USAGE="Usage: $(basename $0) [-v | --verbose] [ gcc | clang ] [ test | bench | reset | clean | debug | release | relwithdebinfo]"
 
 CMAKE=cmake
 BUILD=./build
 TYPE=debug
 COMPILER=clang
 TEST=
+BENCH=
 CLEAN=
 RESET=
 VERBOSE=
@@ -24,7 +25,7 @@ for arg; do
     clang)        COMPILER=clang ;;
     gcc)          COMPILER=gcc ;;
     clean)        CLEAN=1 ;;
-    test)         TEST="-DARRCMP_TEST=ON";;
+    bench)        BENCH="-DHIBP_BENCH=ON";;
     reset)        RESET=1 ;;
     *)            echo -e "unknown option $arg\n$USAGE" >&2;  exit 1 ;;
   esac
@@ -36,14 +37,31 @@ BUILD_DIR=$BUILD/$COMPILER/$TYPE
 
 if [[ "$COMPILER" == "clang" ]]
 then
-    COMPILER_OPTIONS="-DCMAKE_C_COMPILER=clang-13 -DCMAKE_CXX_COMPILER=clang++-13"
+    # detect version specific preferred name
+    if command -v clang-13 &> /dev/null
+    then
+      C_COMPILER=clang-13 
+      CXX_COMPILER=clang++-13
+    else
+      C_COMPILER=clang 
+      CXX_COMPILER=clang++
+    fi
 else
-    COMPILER_OPTIONS="-DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11"
+    # detect version specific preferred name
+    if command -v gcc-11 &> /dev/null
+    then
+      C_COMPILER=gcc-11
+      CXX_COMPILER=g++-11
+    else
+      C_COMPILER=gcc 
+      CXX_COMPILER=g++
+    fi
 fi
+COMPILER_OPTIONS="-DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER"
 
 [[ -n $RESET && -d $BUILD_DIR ]] && rm -rf $BUILD_DIR
     
-$CMAKE -S . -B $BUILD_DIR $COMPILER_OPTIONS -DCMAKE_BUILD_TYPE=$TYPE $TEST
+$CMAKE -S . -B $BUILD_DIR $COMPILER_OPTIONS -DCMAKE_BUILD_TYPE=$TYPE $TEST $BENCH
 
 [[ -n $CLEAN ]] && $CMAKE --build $BUILD_DIR --target clean
 
