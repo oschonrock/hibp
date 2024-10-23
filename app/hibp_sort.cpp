26c63510c3ae618ea95fd915ed358d97c48edb23
Oliver Schonrock <oliver@maxwellenergy.co.uk>
2024-10-23 19:56:23 +0000

    remove unncessary includes
    remove commented code
    more conservative mem usage

--- app/hibp_sort.cpp
+++ app/hibp_sort.cpp
@@ -1,18 +1,9 @@
 #include "flat_file.hpp"
 #include "hibp.hpp"
-#include "os/bch.hpp"
-#include <cstddef>
 #include <cstdlib>
-#include <filesystem>
-#include <functional>
 #include <iostream>
-#include <list>
-#include <numeric>
-#include <ranges>
 #include <stdexcept>
 #include <string>
-#include <type_traits>
-#include <vector>
 
 int main(int argc, char* argv[]) {
 
@@ -22,34 +13,7 @@ int main(int argc, char* argv[]) {
     std::string                          in_filename(argv[1]);
     flat_file::database<hibp::pawned_pw> db(in_filename, 1'000);
 
-    // auto sorted_filename = flat_file::disksort_range<hibp::pawned_pw>(
-    //     db.begin(), db.begin() + 100, {}, {}, 100); // std::greater<>{}, &hibp::pawned_pw::count,
-    //     100);
-
-    auto sorted_filename = db.disksort(); // std::greater<>{}, &hibp::pawned_pw::count);
-    std::vector<std::string> chunk_filenames{
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0000",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0001",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0002",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0003",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0004",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0005",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0006",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0007",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0008",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0009",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0010",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0011",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0012",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0013",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0014",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0015",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0016",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0017",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0018",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0019",
-        "../pwned-passwords-sha1-ordered-by-count-v8.bin.partial.0020",
-    };
+    auto sorted_filename = db.disksort({}, {}, 500'000'000);
     
     std::cerr << "Done. Sorted data was written to " << sorted_filename << "\n";
 
