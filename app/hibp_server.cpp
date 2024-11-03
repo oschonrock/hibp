f4a9bf9df2a0ba80cafae9431d0c5555f246d3cd
Oliver Schonrock <oliver@maxwellenergy.co.uk>
2024-11-03 11:35:04 +0000

    forgot to comment

--- app/hibp_server.cpp
+++ app/hibp_server.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]) {
       SHA1 sha1;
       sha1.update(std::string(params["password"]));
       // for performance testing
-      sha1.update(std::string(params["password"]) + std::to_string(uniq++));
+      // sha1.update(std::string(params["password"]) + std::to_string(uniq++));
       hibp::pawned_pw needle = hibp::convert_to_binary(sha1.final());
 
       std::optional<hibp::pawned_pw> maybe_ppw;
