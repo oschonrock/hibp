a6d0e39289d45d6456d6d22a03adc7a9864d9d5f
Oliver Schonrock <oliver@schonrocks.com>
2024-11-14 20:06:29 +0000

    add dependency fetch script (mainly for snapcraft)

---
+++ fetch_deps.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git submodule update --init --recursive
+cd ext/restinio
+sudo gem install Mxx_ru   # install ruby gem required for restinio dependency installation
+mxxruexternals            # install those deps
+cd ../..
