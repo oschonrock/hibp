b84f78b53d9d61c4b6f3c31373a0435ce36b3a6f
Oliver Schonrock <oliver@schonrocks.com>
2024-11-19 17:17:12 +0000

    tweaks to release script

--- release.sh
+++ release.sh
@@ -22,15 +22,17 @@ rm -rf $NEW_PACKAGE_DIR
 mkdir -p $NEW_PACKAGE_DIR/DEBIAN/
 cp DEBIAN/control $NEW_PACKAGE_DIR/DEBIAN/
 
-git pull
-./build.sh -c gcc -b release --install --install-prefix=$NEW_PACKAGE_DIR/usr/local &&
+git pull &&
+    ./build.sh -c gcc -b release --purge --nopch --install --install-prefix=$NEW_PACKAGE_DIR/usr/local &&
     cd $HOME &&
     dpkg-deb --build --root-owner-group $NEW_PACKAGE_DIR &&
     cd hibp
 
 echo "package built, if all works nicely do this:"
+echo
 echo "git tag v$NEW_VERSION"
 echo "git commit -m\"releasing new version $NEW_VERSION\"" DEBIAN/control README.md
+echo "git push"
 echo "git push origin --tags"
 echo
 echo "then create a new release on github and upload the .deb"
