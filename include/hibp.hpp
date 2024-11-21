75b95d78c90a6e9c870bb8cb1791d0cdd408a0f0
Oliver Schonrock <oliver@schonrocks.com>
2024-11-21 01:07:50 +0000

    namespacing pawned_pw
    and pre-post condition tweaks

--- include/hibp.hpp
+++ include/hibp.hpp
@@ -16,17 +16,36 @@ struct pawned_pw;
 
 inline pawned_pw convert_to_binary(const std::string& text);
 
+namespace detail {
+  
+constexpr std::byte make_nibble(char c) {
+  assert((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'));
+  auto nibble = c - '0';
+  if (nibble > 9) nibble = (nibble & ~('a' - 'A')) - ('A' - '0') + 10; // NOLINT signed
+  assert(nibble >= 0 && nibble <= 15);
+  return static_cast<std::byte>(nibble);
+}
+
+constexpr std::byte make_byte(char mschr, char lschr) {
+  return make_nibble(mschr) << 4U | make_nibble(lschr);
+}
+
+constexpr std::byte make_byte(const char* two_chrs) {
+  return make_byte(*two_chrs, *(two_chrs + 1));
+}
+
 constexpr char nibble_to_char(std::byte nibble) {
   auto n = std::to_integer<uint8_t>(nibble);
   assert(n <= 15);
   return static_cast<char>(n + (n < 10 ? '0' : 'A' - 10));
 }
+} // namespace detail
 
 struct pawned_pw {
   pawned_pw() = default;
 
-  pawned_pw(const std::string& text) // NOLINT implicit conversion
-      : pawned_pw(hibp::convert_to_binary(text)) {}
+  pawned_pw(const std::string& text) // NOLINT detailicit conversion
+      : pawned_pw(convert_to_binary(text)) {}
 
   std::strong_ordering operator<=>(const pawned_pw& rhs) const {
     return arrcmp::array_compare(hash, rhs.hash, arrcmp::three_way{});
@@ -40,8 +59,8 @@ struct pawned_pw {
     std::string buffer(60, '\0');
     char*       strptr = buffer.data();
     for (auto h: hash) {
-      *strptr++ = nibble_to_char(h >> 4U);
-      *strptr++ = nibble_to_char(h & std::byte(0x0FU));
+      *strptr++ = detail::nibble_to_char(h >> 4U);
+      *strptr++ = detail::nibble_to_char(h & std::byte(0x0FU));
     }
     *strptr++      = ':';
     auto [ptr, ec] = std::to_chars(strptr, buffer.data() + buffer.size(), count);
@@ -57,30 +76,15 @@ struct pawned_pw {
   std::int32_t              count; // important to be definitive about size
 };
 
-constexpr std::byte make_nibble(char nibblechr) {
-  auto nibble = nibblechr - '0';
-  if (nibble > 9) nibble = (nibble & ~('a' - 'A')) - ('A' - '0') + 10; // NOLINT signed
-  assert(nibble >= 0 && nibble <= 15);
-  return static_cast<std::byte>(nibble);
-}
-
-constexpr std::byte make_byte(char mschr, char lschr) {
-  return make_nibble(mschr) << 4U | make_nibble(lschr);
-}
-
-constexpr std::byte make_byte(const char* two_chrs) {
-  return make_byte(*two_chrs, *(two_chrs + 1));
-}
-
 // `text` must be an uppper- or lowercase sha1 hexstr
 // with optional ":123" appended (123 is the count).
 inline pawned_pw convert_to_binary(const std::string& text) {
   pawned_pw ppw; // NOLINT initlialisation not needed here
 
-  assert(text.length() >= ppw.hash.size() * 2); // NOLINT assert array to ptr
+  assert(text.length() >= ppw.hash.size() * 2);
   std::size_t i = 0;
   for (auto& b: ppw.hash) {
-    b = make_byte(&text[2 * i]);
+    b = detail::make_byte(&text[2 * i]);
     ++i;
   }
 
@@ -91,5 +95,4 @@ inline pawned_pw convert_to_binary(const std::string& text) {
   }
   return ppw;
 }
-
 } // namespace hibp
