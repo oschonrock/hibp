9f9024aef43592b269e3c33ad2c972b6f73cb5f0
Oliver Schonrock <oliver@schonrocks.com>
2024-11-25 08:57:36 +0000

    use vars for long dirs and add --toc tests

--- test/system_tests.sh
+++ test/system_tests.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 projdir="$(realpath $(dirname $0)/..)"
 
 if [[ -z $1 ]]
@@ -13,13 +12,14 @@ shift
 
 oneTimeSetUp() {
     export tmpdir=$projdir/test/tmp
+    export datadir=$projdir/test/data
     mkdir -p $tmpdir
     rm -rf $tmpdir/*
 
     stdoutF="${tmpdir}/stdout"
     stderrF="${tmpdir}/stderr"
 
-    $builddir/mock_api_server $projdir/test/data &
+    $builddir/mock_api_server $datadir &
     export mock_server_pid=$!
     
     export avoidDoubleTearDownExecution="true"
@@ -29,7 +29,7 @@ oneTimeTearDown() {
     if [[ "${avoidDoubleTearDownExecution}" == "true" ]]
     then   
 	kill $mock_server_pid
-	rm -rf $projdir/test/tmp/*
+	rm -rf $tmpdir/*
 	unset -v avoidDoubleTearDownExecution
     fi
 }
@@ -49,13 +49,13 @@ th_assertTrueWithNoOutput() {
 # download
 
 testDownloadSha1() {
-    $builddir/hibp-download --testing $projdir/test/tmp/hibp_test.sha1.bin --limit 256 --no-progress >${stdoutF} 2>${stderrF}
+    $builddir/hibp-download --testing $tmpdir/hibp_test.sha1.bin --limit 256 --no-progress >${stdoutF} 2>${stderrF}
     rtrn=$?
     th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
 
 testDownloadNtlm() {
-    $builddir/hibp-download --testing $projdir/test/tmp/hibp_test.ntlm.bin --ntlm --limit 256 --no-progress >${stdoutF} 2>${stderrF}
+    $builddir/hibp-download --testing $tmpdir/hibp_test.ntlm.bin --ntlm --limit 256 --no-progress >${stdoutF} 2>${stderrF}
     rtrn=$?
     th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
@@ -63,13 +63,13 @@ testDownloadNtlm() {
 # check download
 
 testDownloadCmpSha1() {
-    cmp $projdir/test/data/hibp_test.sha1.bin $projdir/test/tmp/hibp_test.sha1.bin >${stdoutF} 2>${stderrF}
+    cmp $datadir/hibp_test.sha1.bin $tmpdir/hibp_test.sha1.bin >${stdoutF} 2>${stderrF}
     rtrn=$?
     th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
 
 testDownloadCmpNtlm() {
-    cmp $projdir/test/data/hibp_test.ntlm.bin $projdir/test/tmp/hibp_test.ntlm.bin >${stdoutF} 2>${stderrF}
+    cmp $datadir/hibp_test.ntlm.bin $tmpdir/hibp_test.ntlm.bin >${stdoutF} 2>${stderrF}
     rtrn=$?
     th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
@@ -78,14 +78,14 @@ testDownloadCmpNtlm() {
 
 testTopnSha1() {
     :> ${stdoutF} 
-    $builddir/hibp-topn test/data/hibp_test.sha1.bin -o test/tmp/hibp_topn.sha1.bin --topn 10000 1>/dev/null  2>${stderrF}
+    $builddir/hibp-topn $datadir/hibp_test.sha1.bin -o $tmpdir/hibp_topn.sha1.bin --topn 10000 1>/dev/null  2>${stderrF}
     rtrn=$?
     th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
 
 testTopnNtlm() {
     :> ${stdoutF} 
-    $builddir/hibp-topn --ntlm test/data/hibp_test.ntlm.bin -o test/tmp/hibp_topn.ntlm.bin --topn 10000 1>/dev/null  2>${stderrF}
+    $builddir/hibp-topn --ntlm $datadir/hibp_test.ntlm.bin -o $tmpdir/hibp_topn.ntlm.bin --topn 10000 1>/dev/null  2>${stderrF}
     rtrn=$?
     th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
@@ -93,13 +93,13 @@ testTopnNtlm() {
 # check topn
 
 testTopnCmpSha1() {
-    cmp $projdir/test/data/hibp_topn.sha1.bin $projdir/test/tmp/hibp_topn.sha1.bin >${stdoutF} 2>${stderrF}
+    cmp $datadir/hibp_topn.sha1.bin $tmpdir/hibp_topn.sha1.bin >${stdoutF} 2>${stderrF}
     rtrn=$?
     th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
 
 testTopnCmpNtlm() {
-    cmp $projdir/test/data/hibp_topn.ntlm.bin $projdir/test/tmp/hibp_topn.ntlm.bin >${stdoutF} 2>${stderrF}
+    cmp $datadir/hibp_topn.ntlm.bin $tmpdir/hibp_topn.ntlm.bin >${stdoutF} 2>${stderrF}
     rtrn=$?
     th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
@@ -109,31 +109,60 @@ testTopnCmpNtlm() {
 testSearchPlainSha1() {
     plain="truelove15"
     correct_count="1002"
-    count=$($builddir/hibp-search $projdir/test/data/hibp_topn.sha1.bin "${plain}" | grep '^found' | cut -d: -f2)
+    count=$($builddir/hibp-search $tmpdir/hibp_topn.sha1.bin "${plain}" | grep '^found' | cut -d: -f2)
     assertEquals "count for plain pw '${plain}' of '${count}' was wrong" "${correct_count}" "${count}"
 }
 
 testSearchPlainNtlm() {
     plain="19696969"
     correct_count="913"
-    count=$($builddir/hibp-search --ntlm $projdir/test/data/hibp_topn.ntlm.bin "${plain}" | grep '^found' | cut -d: -f2)
+    count=$($builddir/hibp-search --ntlm $tmpdir/hibp_topn.ntlm.bin "${plain}" | grep '^found' | cut -d: -f2)
     assertEquals "count for plain pw '${plain}' of '${count}' was wrong" "${correct_count}" "${count}"
 }
 
 testSearchHashSha1() {
     hash="00001131628B741FF755AAC0E7C66D26A7C72082"
     correct_count="1002"
-    count=$($builddir/hibp-search --hash $projdir/test/data/hibp_topn.sha1.bin "${hash}" | grep '^found' | cut -d: -f2)
+    count=$($builddir/hibp-search --hash $tmpdir/hibp_topn.sha1.bin "${hash}" | grep '^found' | cut -d: -f2)
     assertEquals "count for hash pw '${hash}' of '${count}' was wrong" "${correct_count}" "${count}"
 }
 
 testSearchHashNtlm() {
     hash="0001256EA8F568DBEACE2E172FD939F7"
     correct_count="913"
-    count=$($builddir/hibp-search --ntlm --hash $projdir/test/data/hibp_topn.ntlm.bin "${hash}" | grep '^found' | cut -d: -f2)
+    count=$($builddir/hibp-search --ntlm --hash $tmpdir/hibp_topn.ntlm.bin "${hash}" | grep '^found' | cut -d: -f2)
     assertEquals "count for hash pw '${hash}' of '${count}' was wrong" "${correct_count}" "${count}"
 }
 
+# search topn with --toc
+
+testSearchPlainSha1Toc() {
+    plain="truelove15"
+    correct_count="1002"
+    count=$($builddir/hibp-search --toc --toc-bits=16 $tmpdir/hibp_topn.sha1.bin "${plain}" | grep '^found' | cut -d: -f2)
+    assertEquals "count for plain pw '${plain}' of '${count}' was wrong" "${correct_count}" "${count}"
+}
+
+testSearchPlainNtlmToc() {
+    plain="19696969"
+    correct_count="913"
+    count=$($builddir/hibp-search --toc --toc-bits=16 --ntlm $tmpdir/hibp_topn.ntlm.bin "${plain}" | grep '^found' | cut -d: -f2)
+    assertEquals "count for plain pw '${plain}' of '${count}' was wrong" "${correct_count}" "${count}"
+}
+
+testSearchHashSha1Toc() {
+    hash="00001131628B741FF755AAC0E7C66D26A7C72082"
+    correct_count="1002"
+    count=$($builddir/hibp-search --toc --toc-bits=16 --hash $tmpdir/hibp_topn.sha1.bin "${hash}" | grep '^found' | cut -d: -f2)
+    assertEquals "count for hash pw '${hash}' of '${count}' was wrong" "${correct_count}" "${count}"
+}
+
+testSearchHashNtlmToc() {
+    hash="0001256EA8F568DBEACE2E172FD939F7"
+    correct_count="913"
+    count=$($builddir/hibp-search --toc --toc-bits=16 --ntlm --hash $tmpdir/hibp_topn.ntlm.bin "${hash}" | grep '^found' | cut -d: -f2)
+    assertEquals "count for hash pw '${hash}' of '${count}' was wrong" "${correct_count}" "${count}"
+}
 
 
 . $projdir/ext/shunit2/shunit2
