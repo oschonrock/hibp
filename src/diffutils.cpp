44b67a5b18f21886f43c489b41708378facfcea2
Oliver Schonrock <oliver@schonrocks.com>
2024-11-27 12:43:30 +0000

    formatting and shorter exception messages

--- src/diffutils.cpp
+++ src/diffutils.cpp
@@ -10,7 +10,8 @@
 namespace hibp::diffutils {
 
 template <hibp::pw_type PwType>
-void run_diff(const std::filesystem::path& old_path,const std::filesystem::path& new_path, std::ostream& diff) {
+void run_diff(const std::filesystem::path& old_path, const std::filesystem::path& new_path,
+              std::ostream& diff) {
   flat_file::database<PwType> db_old(old_path, (1U << 16U) / sizeof(PwType));
   flat_file::database<PwType> db_new(new_path, (1U << 16U) / sizeof(PwType));
 
@@ -26,40 +27,44 @@ void run_diff(const std::filesystem::path& old_path,const std::filesystem::path&
       // copy rest of new into diff as inserts
       while (diff_iter_new != db_new.end()) {
         diff << fmt::format("I:{:08X}:{}\n", diff_iter_old - db_old.begin(),
-                                 diff_iter_new->to_string());
+                            diff_iter_new->to_string());
         ++diff_iter_new;
       }
       break;
     }
     if (diff_iter_new == db_new.end()) {
-      throw std::runtime_error("NEW was shorter. This shouldn't happen!");
+      throw std::runtime_error("NEW was shorter");
     }
     // fine to dereference both
 
-    if (std::next(diff_iter_old) != db_old.end() && deep_equals(*std::next(diff_iter_old), *diff_iter_new)) {
-      throw std::runtime_error("Deletion from OLD. This shouldn't happen!");
+    if (std::next(diff_iter_old) != db_old.end() &&
+        deep_equals(*std::next(diff_iter_old), *diff_iter_new)) {
+      throw std::runtime_error("Deletion from OLD");
     }
     // fine to dereference std::next(new)
-    if (std::next(diff_iter_new) != db_new.end() && deep_equals(*diff_iter_old, *(diff_iter_new + 1))) {
+    if (std::next(diff_iter_new) != db_new.end() &&
+        deep_equals(*diff_iter_old, *(diff_iter_new + 1))) {
       diff << fmt::format("I:{:08X}:{}\n", diff_iter_old - db_old.begin(),
-                               diff_iter_new->to_string());
+                          diff_iter_new->to_string());
       old_begin = diff_iter_old;
       new_begin = diff_iter_new + 1;
       continue;
     }
     if (*diff_iter_old != *diff_iter_new) { // comparing hash only
-      throw std::runtime_error("An update which is not just count. "
-                               "Shouldn't happen, as it implies deletion from OLD.");
+      throw std::runtime_error("Replacement implies deletion");
     }
     diff << fmt::format("U:{:08X}:{}\n", diff_iter_old - db_old.begin(),
-                             diff_iter_new->to_string());
+                        diff_iter_new->to_string());
     old_begin = diff_iter_old + 1;
     new_begin = diff_iter_new + 1;
   }
 }
 
+template void run_diff<hibp::pawned_pw_sha1>(const std::filesystem::path& old_path,
+                                             const std::filesystem::path& new_path,
+                                             std::ostream&                diff);
+template void run_diff<hibp::pawned_pw_ntlm>(const std::filesystem::path& old_path,
+                                             const std::filesystem::path& new_path,
+                                             std::ostream&                diff);
 
-template void run_diff<hibp::pawned_pw_sha1>(const std::filesystem::path& old_path, const std::filesystem::path& new_path, std::ostream& diff);
-template void run_diff<hibp::pawned_pw_ntlm>(const std::filesystem::path& old_path, const std::filesystem::path& new_path, std::ostream& diff);
-
-}  // namespace hibp::diffutils
+} // namespace hibp::diffutils
