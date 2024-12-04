5106c6b4fbd1e94cd695f523332ad4cf3e56061e
Oliver Schonrock <oliver@schonrocks.com>
2024-12-04 15:00:02 +0000

    initial barebones of binaryfusefilter
    selected libraries we want
    
    copy pasta code made to work
    
    noteven sure we will keep these cli apps

---
+++ app/hibp_build_filter.cpp
@@ -0,0 +1,182 @@
+#include "arrcmp.hpp"
+#include "binaryfusefilter.h"
+#include "flat_file.hpp"
+#include "hibp.hpp"
+#include <CLI/CLI.hpp>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <filesystem>
+#include <fmt/format.h>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+
+struct cli_config_t {
+  std::string output_filename;
+  std::string input_filename;
+  bool        force  = false;
+  bool        ntlm   = false;
+  bool        verify = true;
+  std::size_t limit  = -1; // ie max
+};
+
+void define_options(CLI::App& app, cli_config_t& cli) {
+
+  app.add_option("-i,--input", cli.input_filename,
+                 "The file that the downloaded binary database will be read from");
+
+  app.add_option("-o,--output", cli.output_filename,
+                 "The file that the downloaded binary database will be written to");
+
+  app.add_option("-l,--limit", cli.limit,
+                 "The maximum number of records that will be converted (default: all)");
+
+  // app.add_flag("--ntlm", cli.ntlm, "Use ntlm hashes rather than sha1.");
+
+  app.add_flag("-f,--force", cli.force, "Overwrite any existing output file!");
+}
+
+std::ifstream get_input_stream(const std::string& input_filename) {
+  auto input_stream = std::ifstream(input_filename);
+  if (!input_stream) {
+    throw std::runtime_error(fmt::format("Error opening '{}' for reading. Because: \"{}\".\n",
+                                         input_filename,
+                                         std::strerror(errno))); // NOLINT errno
+  }
+  return input_stream;
+}
+
+std::ofstream get_output_stream(const std::string& output_filename, bool force) {
+  if (!force && std::filesystem::exists(output_filename)) {
+    throw std::runtime_error(
+        fmt::format("File '{}' exists. Use `--force` to overwrite.", output_filename));
+  }
+
+  auto output_stream = std::ofstream(output_filename, std::ios_base::binary);
+  if (!output_stream) {
+    throw std::runtime_error(fmt::format("Error opening '{}' for writing. Because: \"{}\".\n",
+                                         output_filename,
+                                         std::strerror(errno))); // NOLINT errno
+  }
+  return output_stream;
+}
+
+void check_options(const cli_config_t& cli) {}
+
+void build(const cli_config_t& cli) {
+  std::istream* input_stream = &std::cin;
+  std::ifstream ifs;
+  ifs                           = get_input_stream(cli.input_filename);
+  input_stream                  = &ifs;
+  std::string input_stream_name = cli.input_filename;
+
+  std::ostream* output_stream = &std::cout;
+  std::ofstream ofs;
+  ofs                            = get_output_stream(cli.output_filename, cli.force);
+  output_stream                  = &ofs;
+  std::string output_stream_name = cli.output_filename;
+
+  flat_file::database<hibp::pawned_pw_sha1> db{cli.input_filename,
+                                               (1U << 16U) / sizeof(hibp::pawned_pw_sha1)};
+
+  unsigned count = 0;
+
+  std::vector<std::uint64_t> hashes;
+  hashes.reserve(std::min(db.number_records(), cli.limit));
+  for (const auto& record: db) {
+    hashes.emplace_back(arrcmp::impl::bytearray_cast<std::uint64_t>(record.hash.data()));
+    count++;
+    if (count == cli.limit) break;
+  }
+
+  auto           start = clock();
+  binary_fuse8_t filter;
+  if (!binary_fuse8_allocate(count, &filter)) {
+    throw std::runtime_error("failed to allocate memory.\n");
+  }
+  if (!binary_fuse8_populate(hashes.data(), count, &filter)) {
+    throw std::runtime_error("failed to build the filter, do you have sufficient memory?\n");
+  }
+  auto end = clock();
+  std::cout << fmt::format("Done in {:.3f} seconds.\n",
+                           static_cast<float>(end - start) / CLOCKS_PER_SEC);
+  if (cli.verify) {
+    std::cout << fmt::format("Checking for false negatives\n");
+    for (size_t i = 0; i < count; i++) {
+      if (!binary_fuse8_contain(hashes[i], &filter)) {
+        throw std::runtime_error("Detected a false negative. You probably have a bug. "
+                                 "Aborting.\n");
+      }
+    }
+    std::cout << fmt::format("Verified with success: no false negatives\n");
+    size_t matches = 0;
+    size_t volume  = 100000;
+    for (size_t t = 0; t < volume; t++) {
+      if (binary_fuse8_contain(t * 10001 + 13 + count, &filter)) {
+        matches++;
+      }
+    }
+    std::cout << fmt::format("estimated false positive rate: {:.3f} percent\n",
+                             static_cast<double>(matches) * 100.0 / static_cast<double>(volume));
+  }
+
+  std::cout << fmt::format("filter will occupy {} bytes\n",
+                           binary_fuse8_serialization_bytes(&filter));
+
+  FILE* write_ptr;
+  write_ptr = fopen(cli.output_filename.c_str(), "wb");
+  if (write_ptr == NULL) {
+    throw std::runtime_error(
+        fmt::format("Cannot write to the output file {}.", cli.output_filename));
+  }
+  uint64_t cookie      = 1234569;
+  bool     isok        = true;
+  size_t   total_bytes = sizeof(cookie) + sizeof(filter.Seed) + sizeof(filter.SegmentLength) +
+                       sizeof(filter.SegmentLengthMask) + sizeof(filter.SegmentCount) +
+                       sizeof(filter.SegmentCountLength) + sizeof(filter.ArrayLength) +
+                       sizeof(uint8_t) * filter.ArrayLength;
+
+  isok &= fwrite(&cookie, sizeof(cookie), 1, write_ptr);
+  isok &= fwrite(&filter.Seed, sizeof(filter.Seed), 1, write_ptr);
+  isok &= fwrite(&filter.SegmentLength, sizeof(filter.SegmentLength), 1, write_ptr);
+  isok &= fwrite(&filter.SegmentLengthMask, sizeof(filter.SegmentLengthMask), 1, write_ptr);
+  isok &= fwrite(&filter.SegmentCount, sizeof(filter.SegmentCount), 1, write_ptr);
+  isok &= fwrite(&filter.SegmentCountLength, sizeof(filter.SegmentCountLength), 1, write_ptr);
+  isok &= fwrite(&filter.ArrayLength, sizeof(filter.ArrayLength), 1, write_ptr);
+  isok &= fwrite(filter.Fingerprints, sizeof(uint8_t) * filter.ArrayLength, 1, write_ptr);
+  isok &= (fclose(write_ptr) == 0);
+  if (isok) {
+    std::cout << fmt::format("filter data saved to {}. Total bytes = {}. \n",
+                             cli.output_filename.c_str(), total_bytes);
+  } else {
+    throw std::runtime_error(
+        fmt::format("failed to write filter data to {}.\n", cli.output_filename));
+  }
+
+  binary_fuse8_free(&filter);
+}
+
+int main(int argc, char* argv[]) {
+  cli_config_t cli;
+
+  CLI::App app("Converting 'Have I been pawned' databases between text and binary formats");
+  define_options(app, cli);
+  CLI11_PARSE(app, argc, argv);
+
+  try {
+    check_options(cli);
+
+    build(cli);
+
+  } catch (const std::exception& e) {
+    std::cerr << fmt::format("Error: {}\n", e.what());
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
---
+++ app/hibp_query_filter.cpp
@@ -0,0 +1,215 @@
+#include "arrcmp.hpp"
+#include "binaryfusefilter.h"
+#include "hibp.hpp"
+#include "sha1.h"
+#include <CLI/CLI.hpp>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <exception>
+#include <filesystem>
+#include <fmt/format.h>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#include <fcntl.h>
+#include <sys/mman.h>
+
+struct cli_config_t {
+  std::string db_filename;
+  std::string plain_text_password;
+  bool        hash = false;
+};
+
+void define_options(CLI::App& app, cli_config_t& cli) {
+
+  app.add_option("db_filename", cli.db_filename, "The file that contains the filter you built.")
+      ->required();
+
+  app.add_option("plain-text-password", cli.plain_text_password,
+                 "The plain text password that you want to check for in the provided hibp_db")
+      ->required();
+
+  app.add_flag("--hash", cli.hash,
+               "Provide a hash on command line, instead of a plaintex password.");
+}
+
+std::ifstream get_input_stream(const std::string& input_filename) {
+  auto input_stream = std::ifstream(input_filename);
+  if (!input_stream) {
+    throw std::runtime_error(fmt::format("Error opening '{}' for reading. Because: \"{}\".\n",
+                                         input_filename,
+                                         std::strerror(errno))); // NOLINT errno
+  }
+  return input_stream;
+}
+
+std::ofstream get_output_stream(const std::string& output_filename, bool force) {
+  if (!force && std::filesystem::exists(output_filename)) {
+    throw std::runtime_error(
+        fmt::format("File '{}' exists. Use `--force` to overwrite.", output_filename));
+  }
+
+  auto output_stream = std::ofstream(output_filename, std::ios_base::binary);
+  if (!output_stream) {
+    throw std::runtime_error(fmt::format("Error opening '{}' for writing. Because: \"{}\".\n",
+                                         output_filename,
+                                         std::strerror(errno))); // NOLINT errno
+  }
+  return output_stream;
+}
+
+void check_options(const cli_config_t& cli) {}
+
+void query(const cli_config_t& cli) {
+
+  std::cout << fmt::format("using database: {}\n", cli.db_filename);
+  uint64_t hexval = 0;
+  if (cli.hash) {
+    throw std::runtime_error("not implemented");
+    // const char* hashhex = argv[optind + 1];
+    // if (strlen(hashhex) != 16) {
+    //   throw std::runtime_error(fmt::format("bad hex. length is %zu, 16 hexadecimal characters
+    //   expected.\n", strlen(hashhex)));
+    // }
+    // uint64_t x1 = hex_to_u32_nocheck((const uint8_t*)hashhex);
+    // uint64_t x2 = hex_to_u32_nocheck((const uint8_t*)hashhex + 4);
+    // uint64_t x3 = hex_to_u32_nocheck((const uint8_t*)hashhex + 8);
+    // uint64_t x4 = hex_to_u32_nocheck((const uint8_t*)hashhex + 12);
+    // if ((x1 | x2 | x3 | x4) > 0xFFFF) {
+    //   std::cout << fmt::format("bad hex value  %s \n", hashhex);
+    //   return EXIT_FAILURE;
+    // }
+    // hexval = (x1 << 48) | (x2 << 32) | (x3 << 16) | x4;
+  } else {
+    std::cout << fmt::format("We are going to hash your input.\n");
+    hibp::pawned_pw_sha1t64 pw{SHA1{}(cli.plain_text_password)};
+    std::cout << pw << "\n";
+    hexval = arrcmp::impl::bytearray_cast<std::uint64_t>(pw.hash.data());
+  }
+  std::cout << fmt::format("hexval = {:016X}\n", hexval);
+  uint64_t cookie = 0;
+
+  uint64_t seed  = 0;
+  clock_t  start = clock();
+
+  // could open the file once (map it), instead of this complicated mess.
+  FILE* fp = fopen(cli.db_filename.c_str(), "rb");
+  if (fp == nullptr) {
+    throw std::runtime_error(fmt::format("Cannot read the input file {}.", cli.db_filename));
+  }
+  // should be done with an enum:
+  bool xor8    = false;
+  bool bin16   = false;
+  bool bloom12 = false;
+  if (fread(&cookie, sizeof(cookie), 1, fp) != 1) std::cout << fmt::format("failed read.\n");
+  if (fread(&seed, sizeof(seed), 1, fp) != 1) std::cout << fmt::format("failed read.\n");
+  // should be a switch/case.
+  if (cookie != 1234569) {
+    if (cookie == 1234570) {
+      bin16 = true;
+    } else {
+      throw std::runtime_error(
+          fmt::format("Not a filter file. Cookie found: %llu.\n", (long long unsigned int)cookie));
+    }
+  }
+  size_t          length = 0;
+  binary_fuse8_t  binfilter;
+  binary_fuse16_t binfilter16;
+
+  if (bin16) {
+    bool isok        = true;
+    binfilter16.Seed = seed;
+    isok &= fread(&binfilter16.SegmentLength, sizeof(binfilter16.SegmentLength), 1, fp);
+    isok &= fread(&binfilter16.SegmentLengthMask, sizeof(binfilter16.SegmentLengthMask), 1, fp);
+    isok &= fread(&binfilter16.SegmentCount, sizeof(binfilter16.SegmentCount), 1, fp);
+    isok &= fread(&binfilter16.SegmentCountLength, sizeof(binfilter16.SegmentCountLength), 1, fp);
+    isok &= fread(&binfilter16.ArrayLength, sizeof(binfilter16.ArrayLength), 1, fp);
+    if (!isok) std::cout << fmt::format("failed read.\n");
+    length = sizeof(cookie) + sizeof(binfilter16.Seed) + sizeof(binfilter16.SegmentLength) +
+             sizeof(binfilter16.SegmentLengthMask) + sizeof(binfilter16.SegmentCount) +
+             sizeof(binfilter16.SegmentCountLength) + sizeof(binfilter16.ArrayLength) +
+             sizeof(uint16_t) * binfilter16.ArrayLength;
+  } else {
+
+    bool isok      = true;
+    binfilter.Seed = seed;
+    isok &= fread(&binfilter.SegmentLength, sizeof(binfilter.SegmentLength), 1, fp);
+    isok &= fread(&binfilter.SegmentLengthMask, sizeof(binfilter.SegmentLengthMask), 1, fp);
+    isok &= fread(&binfilter.SegmentCount, sizeof(binfilter.SegmentCount), 1, fp);
+    isok &= fread(&binfilter.SegmentCountLength, sizeof(binfilter.SegmentCountLength), 1, fp);
+    isok &= fread(&binfilter.ArrayLength, sizeof(binfilter.ArrayLength), 1, fp);
+    if (!isok) std::cout << fmt::format("failed read.\n");
+    length = sizeof(cookie) + sizeof(binfilter.Seed) + sizeof(binfilter.SegmentLength) +
+             sizeof(binfilter.SegmentLengthMask) + sizeof(binfilter.SegmentCount) +
+             sizeof(binfilter.SegmentCountLength) + sizeof(binfilter.ArrayLength) +
+             sizeof(uint8_t) * binfilter.ArrayLength;
+  }
+  if (bin16)
+    std::cout << fmt::format("16-bit binary fuse filter detected.\n");
+  else
+    std::cout << fmt::format("8-bit binary fuse filter detected.\n");
+  fclose(fp);
+  int  fd     = open(cli.db_filename.c_str(), O_RDONLY);
+  bool shared = false;
+
+  std::cout << fmt::format("I expect the file to span {} bytes.\n", length);
+  uint8_t* addr = (uint8_t*)(mmap(NULL, length, PROT_READ,
+                                  MAP_FILE | (shared ? MAP_SHARED : MAP_PRIVATE), fd, 0));
+  if (addr == MAP_FAILED) {
+    throw std::runtime_error(fmt::format("Data can't be mapped???\n"));
+  } else {
+    std::cout << fmt::format("memory mapping is a success.\n");
+  }
+  if (bin16) {
+    binfilter16.Fingerprints = reinterpret_cast<uint16_t*>(
+        addr + sizeof(cookie) + sizeof(binfilter16.Seed) + sizeof(binfilter16.SegmentLength) +
+        sizeof(binfilter16.SegmentLengthMask) + sizeof(binfilter16.SegmentCount) +
+        sizeof(binfilter16.SegmentCountLength) + sizeof(binfilter16.ArrayLength));
+    if (binary_fuse16_contain(hexval, &binfilter16)) {
+      std::cout << fmt::format("Probably in the set.\n");
+    } else {
+      std::cout << fmt::format("Surely not in the set.\n");
+    }
+  } else {
+    binfilter.Fingerprints = addr + sizeof(cookie) + sizeof(binfilter.Seed) +
+                             sizeof(binfilter.SegmentLength) + sizeof(binfilter.SegmentLengthMask) +
+                             sizeof(binfilter.SegmentCount) + sizeof(binfilter.SegmentCountLength) +
+                             sizeof(binfilter.ArrayLength);
+    if (binary_fuse8_contain(hexval, &binfilter)) {
+      std::cout << fmt::format("Probably in the set.\n");
+    } else {
+      std::cout << fmt::format("Surely not in the set.\n");
+    }
+  }
+  clock_t end = clock();
+
+  std::cout << fmt::format("Processing time {:.3f} microseconds.\n",
+                           (float)(end - start) * 1000.0 * 1000.0 / CLOCKS_PER_SEC);
+  std::cout << fmt::format("Expected number of queries per second: {:.3f} \n",
+                           (float)CLOCKS_PER_SEC / (end - start));
+  munmap(addr, length);
+}
+
+int main(int argc, char* argv[]) {
+  cli_config_t cli;
+
+  CLI::App app("Converting 'Have I been pawned' databases between text and binary formats");
+  define_options(app, cli);
+  CLI11_PARSE(app, argc, argv);
+
+  try {
+    check_options(cli);
+
+    query(cli);
+
+  } catch (const std::exception& e) {
+    std::cerr << fmt::format("Error: {}\n", e.what());
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
